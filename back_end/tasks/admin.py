27630217895eb3fb58c729dc932647be71cfe1ba
rabbit463 <rabbit463@users.noreply.github.com>
2021-04-11 18:30:07 -0400

    updated admin interface

--- back_end/tasks/admin.py
+++ back_end/tasks/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Task
 
 # Register your models here.
-admin.site.register(Task)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('content', 'created', "completed",
+                    "user", 'completedTime', 'order')
+
+    search_fields = ['content', 'user']
+    list_filter = ('completed', 'created', 'completedTime', 'order')
+
+    fields = ('content', ('completed',
+              'completedTime'), ('user', 'order'))
+
+    readonly_fields = ('created',)
+
+
+admin.site.register(Task, TaskAdmin)
